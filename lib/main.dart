740d44aa0dc4b824728f19e8901c99a9cf90e80f
Last-dreamer <asim0000.ak@gmail.com>
2023-08-12 20:27:01 +0400

    adding firebase script

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:motion/motion.dart';
 import 'package:portfolio/resource/colors.dart';
 import 'package:portfolio/view/root.dart';
+
+import 'firebase_options.dart';
  
  
 void main()async {
 
   // setPathUrlStrategy();
+  await Firebase.initializeApp(
+
+    options: DefaultFirebaseOptions.currentPlatform,
+
+);
   WidgetsFlutterBinding.ensureInitialized(); 
   await Motion.instance.initialize();
   Motion.instance.setUpdateInterval(60.fps);
-
   runApp(ProviderScope(child: const AppTheme()));
-
-
 }
 
 class AppTheme extends StatelessWidget {
